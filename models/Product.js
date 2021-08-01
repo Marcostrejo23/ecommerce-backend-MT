7a98c388c29bcf616b87ab6f0acebf0254c8b62c
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-01 16:41:32 -0700

    pushing model product.js completed

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,6 +9,33 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    stock: {
+      type: dayaTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes: INTEGER,
+      model: 'category',
+      allowNull: false,
+      key: 'id',
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
     // define columns
   },
   {
--- models/Tag.js
+++ models/Tag.js
@@ -6,25 +6,17 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true, 
-    }
-  },
-  tag_name: {
-    type:DataTypes.STRING,
-    allowNull: false,
-  },
 
+  tag_name:{
+    type: DataTypes.STRING,
+  },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'tag',
-  }
+  },
 );
 
 module.exports = Tag;
