1578f62d9fb541d740c951faba925bf1423e31d0
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-01 16:55:03 -0700

    product tag finished

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,11 +1,26 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        key: `id`,
+        model: Product,
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
     // define columns
   },
   {
