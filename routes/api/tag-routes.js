439ff46b6e9c7b39cd0edb98eb0b0d6923f85086
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-22 09:56:47 -0700

    update tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,12 +29,27 @@ router.get('/:id', (req, res) =>{
     res.status(400).json(err)
   }; 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag)
+  }catch(err){
+    res.status(400).json(err)
+  }
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try{
+    const updateTag = await Tag.update(req.body,{
+      where: {id:req.params.id},
+    })
+    res.status(200).json({message:updateTag[0] ? `Tag updated` :`no tag found.` })
+  }catch(err){
+    res.status(400).json(err)
+  }
   // update a tag's name by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
